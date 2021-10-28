ca5ccdc963466ee3f210f80a067a3160c4790633
mesutkoc <mestkoc34@gmail.com>
2021-10-29 02:22:38 +0300

    add product component for each item and hover

---
+++ src/components/Body/Productside/ProductComponent.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "./style.css";
+import Image1 from "../../../assets/image1.png";
+// import Image2 from "../../../assets/image2.png";
+// import Image3 from "../../../assets/image3.png";
+// import Image4 from "../../../assets/image4.png";
+
+function ProductComponent(item) {
+  const [hover, setHover] = useState({ id: null, hovers: null });
+  const calculateDiscount = (price, discount) => {
+    const discountCash = (parseInt(price) * parseInt(discount)) / 100;
+    const newPrice = price - discountCash;
+    return [newPrice, discountCash];
+  };
+  const onMouseEnterHandler = (id) => {
+    setHover({ id: id, hovers: true });
+  };
+  const onMouseLeaveHandler = () => {
+    setHover({});
+  };
+  return (
+    <div>
+      <div
+        key={item.props.id}
+        className={`item ${hover.id === item.props.id && hover.hovers}`}
+        onMouseEnter={() => onMouseEnterHandler(item.props.id)}
+        onMouseLeave={() => onMouseLeaveHandler()}
+      >
+        <div
+          className={`itemImageDiv ${
+            hover.id === item.props.id && hover.hovers
+          }`}
+        >
+          <img src={Image1} alt="item1" className="itemImage"></img>
+        </div>
+        <div className="itemDetail">
+          <p className="itemDetailDescription">{item.props.description}</p>
+
+          <div
+            className={`brandDiv ${hover.id === item.props.id && hover.hovers}`}
+          >
+            <div className="brandDetail">
+              <span className="itemBrand">
+                Marka:
+                <span className="itemModelDetail">{item.props.brand}</span>
+              </span>
+              <span className="itemColor">
+                Renk:<span className="itemModelDetail">{item.props.color}</span>
+              </span>
+            </div>
+            <div className="priceDetail">
+              <span className="itemPrice">
+                {calculateDiscount(item.props.price, item.props.discount)[0]} TL
+              </span>
+              {parseInt(item.props.discount) !== 0 && (
+                <span className="discount">
+                  <span className="itemDiscount">
+                    {
+                      calculateDiscount(
+                        item.props.price,
+                        item.props.discount
+                      )[1] + " TL"
+                    }
+                  </span>
+                  <span className="itemDiscountDetail">
+                    {parseInt(item.props.discount) !== 0 &&
+                      item.props.discount + "%"}
+                  </span>
+                </span>
+              )}
+            </div>
+          </div>
+          <div
+            className={`addBasketButton ${
+              hover.id === item.props.id && hover.hovers
+            }`}
+          >
+            <button className="addButton" onClick={() => console.log("done")}>
+              Sepete Ekle
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductComponent;
--- src/components/Body/Productside/index.js
+++ src/components/Body/Productside/index.js
@@ -1,82 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
-import Image1 from "../../../assets/image1.png";
-// import Image2 from "../../../assets/image2.png";
-// import Image3 from "../../../assets/image3.png";
-// import Image4 from "../../../assets/image4.png";
+
 import { useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
 
 function Productside() {
-  const [hover, setHover] = useState(false);
+
   const product = useSelector((state) => state.products.productsData);
 
-  const calculateDiscount = (price, discount) => {
-    const discountCash = (parseInt(price) * parseInt(discount)) / 100;
-    const newPrice = price - discountCash;
-    return [newPrice, discountCash];
-  };
-  const onMouseEnterHandler = (e) => {
-    console.log(e);
-    setHover(true);
-  };
-  const onMouseLeaveHandler = (e) => {
-    console.log(e);
-    setHover(false);
-  };
   return (
     <div className="itemSide">
       <div className="items">
         {product.map((item) => (
-          <div
-            key={item.id}
-            className="item "
-            onMouseEnter={() => onMouseEnterHandler(item.id)}
-            onMouseLeave={() => onMouseLeaveHandler(item.id)}
-          >
-            <div className="itemImageDiv">
-              <img src={Image1} alt="item1" className="itemImage"></img>
-            </div>
-            <div className="itemDetail">
-              <p className="itemDetailDescription">{item.description}</p>
-              {hover === false ? (
-                <div className="brandDiv">
-                  <div className="brandDetail">
-                    <span className="itemBrand">
-                      Marka:
-                      <span className="itemModelDetail">{item.brand}</span>
-                    </span>
-                    <span className="itemColor">
-                      Renk:<span className="itemModelDetail">{item.color}</span>
-                    </span>
-                  </div>
-                  <div className="priceDetail">
-                    <span className="itemPrice">
-                      {calculateDiscount(item.price, item.discount)[0]} TL
-                    </span>
-                    {parseInt(item.discount) !== 0 && (
-                      <span className="discount">
-                        <span className="itemDiscount">
-                          {calculateDiscount(item.price, item.discount)[1]}
-                        </span>
-                        <span className="itemDiscountDetail">
-                          {parseInt(item.discount) !== 0 && item.discount + "%"}
-                        </span>
-                      </span>
-                    )}
-                  </div>
-                </div>
-              ) : (
-                <div className="addBasketButton">
-                  <button
-                    className="addButton"
-                    onClick={() => console.log("done")}
-                  >
-                    Sepete Ekle
-                  </button>
-                </div>
-              )}
-            </div>
-          </div>
+          <ProductComponent props={item} key={item.id}></ProductComponent>
         ))}
       </div>
     </div>
