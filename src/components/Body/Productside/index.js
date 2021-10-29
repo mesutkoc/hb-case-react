680e4caf94c9451009ec6828b9b1aaceb7037a83
mesutkoc <mestkoc34@gmail.com>
2021-10-30 01:11:57 +0300

    add pagination

--- src/components/Body/Productside/index.js
+++ src/components/Body/Productside/index.js
@@ -1,20 +1,43 @@
 import React from "react";
 import "./style.css";
-
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setCurrentPage } from "redux/slices/Products/";
 import ProductComponent from "./ProductComponent";
 
 function Productside() {
-
+  const dispatch = useDispatch();
   const product = useSelector((state) => state.products.productsData);
+  const currentPage = useSelector((state) => state.products.currentPage);
+  const productPerPage = useSelector((state) => state.products.productPerPage);
 
+  const indexOfLastProduct = currentPage * productPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productPerPage;
+  const currentProduct = product.slice(indexOfFirstProduct, indexOfLastProduct);
+  const renderProducts = currentProduct.map((product, index) => {
+    return (
+      <ProductComponent props={product} key={product.id}></ProductComponent>
+    );
+  });
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(product.length / productPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  const changePage = (data) => {
+    dispatch(setCurrentPage(data))
+  }
+  const renderPageNumbers = pageNumbers.map((number) => {
+    return (
+      <div key={number} id={number} onClick={(e)=>changePage(e.target.id)}>
+        {number}
+      </div>
+    );
+  });
   return (
     <div className="itemSide">
       <div className="items">
-        {product.map((item) => (
-          <ProductComponent props={item} key={item.id}></ProductComponent>
-        ))}
+        {renderProducts}
       </div>
+      {renderPageNumbers}
     </div>
   );
 }
--- src/redux/slices/Products/index.js
+++ src/redux/slices/Products/index.js
@@ -3,9 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 export const productSlice = createSlice({
     name: 'products',
     initialState: {
-        productsData:JSON.parse(localStorage.getItem("productList"))
+        productsData: JSON.parse(localStorage.getItem("productList")),
+        currentPage: 1,
+        productPerPage:12,
     },
-    reducers:{}
+    reducers: {
+        setCurrentPage: (state, action) => {
+            console.log(action.payload)
+            state.currentPage = action.payload
+        }
+    }
 })
-
+export const { setCurrentPage } = productSlice.actions;
 export default productSlice.reducer;
